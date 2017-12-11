9d7befb570f435fd8fcabc5887956d2981c3a3c3
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-12-10 16:46:04 -0800

    添加class, html文件

--- 问好.ts
+++ 问好.ts
@@ -1,3 +1,10 @@
+class 学生 {
+  全名: string;
+  constructor(public 姓: string, public 名: string) {
+      this.全名 = 姓 + 名;
+  }
+}
+
 interface 人 {
   姓: string;
   名: string;
@@ -7,6 +14,6 @@ function 问好(那谁: 人) {
   return "吃了么, " + 那谁.姓 + 那谁.名;
 }
 
-let 路人 = {姓: "大", 名: "黄"};
+let 路人 = new 学生("大", "黄");
 
 document.body.innerHTML = 问好(路人);
