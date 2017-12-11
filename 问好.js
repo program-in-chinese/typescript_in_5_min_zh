a3631558044e5c7642f99a3b0fcc57af79133db6
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-12-10 16:29:03 -0800

    检验类型检查 - 问好.ts(7,30): error TS2345: Argument of type 'number[]' is not assignable to parameter of type 'string'.

--- 问好.js
+++ 问好.js
@@ -1,5 +1,5 @@
 function 问好(那谁) {
     return "吃了么, " + 那谁;
 }
-var 路人 = "打酱油的";
+var 路人 = [0, 1, 2];
 document.body.innerHTML = 问好(路人);
--- 问好.ts
+++ 问好.ts
@@ -2,6 +2,6 @@ function 问好(那谁: string) {
   return "吃了么, " + 那谁;
 }
 
-let 路人 = "打酱油的";
+let 路人 = [0, 1, 2];
 
 document.body.innerHTML = 问好(路人);
