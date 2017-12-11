61515191e135b1d7248d459f9853433aec6daef9
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-12-10 16:34:22 -0800

    接口示例

--- 问好.js
+++ 问好.js
@@ -1,5 +1,5 @@
 function 问好(那谁) {
-    return "吃了么, " + 那谁;
+    return "吃了么, " + 那谁.姓 + 那谁.名;
 }
-var 路人 = [0, 1, 2];
+var 路人 = { 姓: "大", 名: "黄" };
 document.body.innerHTML = 问好(路人);
--- 问好.ts
+++ 问好.ts
@@ -1,7 +1,12 @@
-function 问好(那谁: string) {
-  return "吃了么, " + 那谁;
+interface 人 {
+  姓: string;
+  名: string;
 }
 
-let 路人 = [0, 1, 2];
+function 问好(那谁: 人) {
+  return "吃了么, " + 那谁.姓 + 那谁.名;
+}
+
+let 路人 = {姓: "大", 名: "黄"};
 
 document.body.innerHTML = 问好(路人);
